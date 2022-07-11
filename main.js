959dae520188c370047d00f8f2dd5365c25c0763
leonrado02 <100576972+leonrado02@users.noreply.github.com>
2022-07-11 13:37:17 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ closeBtn.addEventListener("click", () => {
 
 const navLinks = document.querySelectorAll(".nav li");
 
-navLinks.forEach(item => {
+navLinks.forEach( item => {
     item.addEventListener("click", () => {
         document.querySelector(".dropdown-menu").classList.remove("active");
     });
@@ -22,3 +22,21 @@ window.addEventListener("scroll", () => {
     document.querySelector(".dropdown-menu").classList.remove("active");
 });
 
+// services reveal
+const questions = document.querySelectorAll(".question");
+const answers = document.querySelectorAll(".answer");
+
+questions[0].addEventListener("click", () => {
+    answers[0].classList.toggle("answer-active");
+    questions[0].classList.toggle("question-active");
+});
+
+questions[1].addEventListener("click", () => {
+    answers[1].classList.toggle("answer-active");
+    questions[1].classList.toggle("question-active");
+});
+
+questions[2].addEventListener("click", () => {
+    answers[2].classList.toggle("answer-active");
+    questions[2].classList.toggle("question-active");
+});
